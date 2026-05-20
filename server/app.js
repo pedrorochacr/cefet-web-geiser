cac2b859bb6847e5c363913293dfefb97069e691
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-05-20 10:20:05 -0300

    renderiza jogador

--- server/app.js
+++ server/app.js
@@ -27,7 +27,67 @@ app.get('/', (req, res) => {
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
-// dica: o handler desta função pode chegar a ter ~15 linhas de código
+app.get('/jogador/:id/', function(req, res) {
+  const id = req.params.id
+
+  // acha o jogador pelo steamid
+  let jogador = null
+  for (let i = 0; i < db.jogadores.players.length; i++) {
+    if (db.jogadores.players[i].steamid === id) {
+      jogador = db.jogadores.players[i]
+      break
+    }
+  }
+
+  const jogosData = db.jogosPorJogador[id]
+
+  if (!jogador || !jogosData) {
+    res.status(404).send('Jogador não encontrado')
+    return
+  }
+
+  // ordena os jogos do mais jogado para o menos jogado
+  const jogos = jogosData.games.slice()
+  jogos.sort(function(a, b) {
+    return b.playtime_forever - a.playtime_forever
+  })
+
+  // conta jogos nunca jogados
+  let naoJogados = 0
+  for (let i = 0; i < jogosData.games.length; i++) {
+    if (jogosData.games[i].playtime_forever === 0) {
+      naoJogados++
+    }
+  }
+
+  // monta o objeto do jogo favorito
+  const jogoFavorito = jogos[0]
+  const favorito = {
+    nome: jogoFavorito.name,
+    horasJogadas: Math.round(jogoFavorito.playtime_forever / 60),
+    imgUrl: 'http://media.steampowered.com/steamcommunity/public/images/apps/' + jogoFavorito.appid + '/' + jogoFavorito.img_logo_url + '.jpg',
+    statsUrl: 'http://steamcommunity.com/profiles/' + id + '/stats/' + jogoFavorito.appid
+  }
+
+  
+  const top5 = []
+  for (let i = 0; i < 5; i++) {
+    const jogo = jogos[i]
+    top5.push({
+      nome: jogo.name,
+      horasJogadas: Math.round(jogo.playtime_forever / 60),
+      imgUrl: 'http://media.steampowered.com/steamcommunity/public/images/apps/' + jogo.appid + '/' + jogo.img_logo_url + '.jpg'
+    })
+  }
+
+  res.render('jogador', {
+    jogador: jogador,
+    quantidadeJogos: jogosData.game_count,
+    naoJogados: naoJogados,
+    favorito: favorito,
+    top5: top5
+  })
+})
 
 
 // EXERCÍCIO 1
